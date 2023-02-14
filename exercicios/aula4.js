6efea761f4609df515e1f43079515d787310ec85
cintia89 <cintia.silvadeooliveira@gmail.com>
2023-02-14 08:49:50 -0300

    pratica

---
+++ exercicios/aula4.js
@@ -0,0 +1,18 @@
+const n1 = 20;
+const n2 = 'alicia';
+const n3 = 10;
+
+console.log(n1, n2, n3)
+
+const país = 'Brasil';
+const pontosTurísticos = 'porto';
+const capital = 'brasilia';
+const quantidade_de_estados = '26';
+const eh_da_América_Latina = 'sim';
+
+console.log(país, 
+pontosTurísticos,
+capital,
+quantidade_de_estados,
+eh_da_América_Latina
+)
